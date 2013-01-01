98d672f7ee458961eedf8f35270fd8ae5464c4e7
mbax <matt@phozop.net>
2013-01-01 16:03:45 -0500

    Don't even need this anymoar

--- src/to/joe/j2mc/anticonn/Monitor.java
+++ src/to/joe/j2mc/anticonn/Monitor.java
@@ -9,37 +9,36 @@ import java.util.regex.Pattern;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPreLoginEvent;
-import org.bukkit.event.player.PlayerPreLoginEvent.Result;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Monitor extends JavaPlugin implements Listener {
     private static final Pattern ipattern = Pattern.compile("(?<=(^|[(\\p{Space}|\\p{Punct})]))((1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\.){3}(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])(?=([(\\p{Space}|\\p{Punct})]|$))");
-    private HashMap<String, Integer> map=new HashMap<String,Integer>();
-    
-    @EventHandler(priority=EventPriority.MONITOR)
-    public void prelogin(PlayerPreLoginEvent event){
-        if(event.getResult()==Result.KICK_BANNED){
-            String ip=event.getAddress().getHostAddress();
-            check(ip);
+    private HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void prelogin(AsyncPlayerPreLoginEvent event) {
+        if (event.getLoginResult() == Result.KICK_BANNED) {
+            check(event.getAddress().getHostAddress());
         }
     }
-    
-    private void check(String ip){
-        if(map.containsKey(ip)){
-            int count=map.get(ip);
+
+    private void check(String ip) {
+        if (map.containsKey(ip)) {
+            int count = map.get(ip);
             count++;
-            if(count>=5){
+            if (count >= 5) {
                 this.getServer().banIP(ip);
-                this.getLogger().info("Banning "+ip+" for spamjoin");
+                this.getLogger().info("Banning " + ip + " for spamjoin");
                 return;
             }
             map.put(ip, count);
-        }else{
+        } else {
             map.put(ip, 1);
         }
     }
-    
+
     @Override
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
@@ -59,8 +58,8 @@ public class Monitor extends JavaPlugin implements Listener {
             }
 
         });
-        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable(){
-            public void run(){
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+            public void run() {
                 map.clear();
             }
         }, 1200, 1200);
